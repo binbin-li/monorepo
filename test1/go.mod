444ba0fa09e9b2a82b747758854e98c95670aac3
Binbin Li <libinbin050215@gmail.com>
2025-01-21 15:15:49 +0000

    feat: add 2 packages
    Signed-off-by: Binbin Li <libinbin050215@gmail.com>

---
+++ test1/go.mod
@@ -0,0 +1,13 @@
+module github.com/binbin-li/monorepo/test1
+
+go 1.23.4
+
+require (
+	github.com/opencontainers/image-spec v1.1.0
+	github.com/ratify-project/ratify-go v0.0.0-20250121002110-2815af359901
+)
+
+require (
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	oras.land/oras-go/v2 v2.5.0 // indirect
+)
---
+++ test1/main.go
@@ -0,0 +1,34 @@
+package test1
+
+import (
+	"context"
+
+	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
+	"github.com/ratify-project/ratify-go"
+)
+
+type Verifier struct{}
+
+func init() {
+	ratify.RegisterVerifier("test1", newVerifier)
+}
+
+func newVerifier(_ ratify.NewVerifierOptions) (ratify.Verifier, error) {
+	return &Verifier{}, nil
+}
+
+func (v *Verifier) Name() string {
+	return "test1"
+}
+
+func (v *Verifier) Type() string {
+	return "test1"
+}
+
+func (v *Verifier) Verifiable(artifact ocispec.Descriptor) bool {
+	return true
+}
+
+func (v *Verifier) Verify(ctx context.Context, opts *ratify.VerifyOptions) (*ratify.VerificationResult, error) {
+	return &ratify.VerificationResult{}, nil
+}
